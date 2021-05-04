96a6f77cb811f7f1661863e018032a96f8c7bded
Amira-Ezz-149 <miraezz14@gmail.com>
2021-05-04 13:02:46 +0200

    handling code

--- lib/Bottom_navigation_bar.dart
+++ lib/Bottom_navigation_bar.dart
@@ -9,21 +9,25 @@ class _BottomNavigationBarState extends State<BottomNavBarClass> {
   int _selectedIndex = 0;
 
   List<Widget> widgetOptions = <Widget>[
-    //style: TextStyle(fontSize: 30),
     Text(
-      'TAB: 0', style: TextStyle(fontSize: 30),
+      'TAB: 0',
+      style: TextStyle(fontSize: 30),
     ),
     Text(
-      'TAB: 1', style: TextStyle(fontSize: 30),
+      'TAB: 1',
+      style: TextStyle(fontSize: 30),
     ),
     Text(
-      'TAB: 2', style: TextStyle(fontSize: 30),
+      'TAB: 2',
+      style: TextStyle(fontSize: 30),
     ),
     Text(
-      'TAB: 3', style: TextStyle(fontSize: 30),
+      'TAB: 3',
+      style: TextStyle(fontSize: 30),
     ),
     Text(
-      'TAB: 4', style: TextStyle(fontSize: 30),
+      'TAB: 4',
+      style: TextStyle(fontSize: 30),
     ),
   ];
 
@@ -41,111 +45,58 @@ class _BottomNavigationBarState extends State<BottomNavBarClass> {
         centerTitle: true,
       ),
       body: Center(
-        //  child : widgetOptns.elementAt(_selectedIndex),
         child: widgetOptions.elementAt(_selectedIndex),
-
-        
       ),
-
 
       //      ==========================>> floatingActionButton
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
 
-    bottomNavigationBar: BottomAppBar(
-  color: Colors.yellow,
-  child:BottomNavigationBar(
-      unselectedItemColor: Colors.blueGrey,
-      selectedItemColor: Colors.red,
-      backgroundColor: Colors.yellow,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.menu,),
-          label: 'This',
-          // backgroundColor: Colors.red
+      bottomNavigationBar: BottomAppBar(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+
+        child: BottomNavigationBar(
+
+          elevation: 20,
+          unselectedItemColor: Colors.blueGrey,
+          selectedItemColor: Colors.red,
+
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.menu,
+              ),
+              label: 'This',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.layers),
+              label: 'Is',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              label: 'A',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.auto_awesome_mosaic),
+              label: 'Bottom',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.report),
+              label: 'Bar',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTap,
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.layers),
-          label: 'Is',
-          //   backgroundColor: Colors.indigo
-
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add, color: Colors.white,),
-          label: 'A',
-          // backgroundColor: Colors.yellow,
-
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.auto_awesome_mosaic),
-          label: 'Bottom',
-          // backgroundColor: Colors.blue,
-
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.report),
-          label: 'Bar',
-          //backgroundColor: Colors.green,
-
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      onTap: _onItemTap,
-      // fixedColor: Colors.indigo,
-       ) ,
-
-),
-
-      //     ===========================>> BottomNavigationBar
-
-/*
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedItemColor: Colors.blueGrey,
-        selectedItemColor: Colors.red,
-        backgroundColor: Colors.yellow,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_rounded,),
-            label: 'profile',
-           // backgroundColor: Colors.red
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-           label: 'settings',
-           //   backgroundColor: Colors.indigo
-
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'add',
-             // backgroundColor: Colors.yellow,
-
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.phone),
-            label: 'phone',
-             // backgroundColor: Colors.blue,
-
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: 'message',
-              //backgroundColor: Colors.green,
-
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTap,
-       // fixedColor: Colors.indigo,
-     , )
-*/
-
+      ),
 
       // ========================>> Location of FloatingActionButton
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-
     );
   }
 }
